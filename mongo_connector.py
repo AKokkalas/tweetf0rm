cd00b1aaeb2c02aad5f3846ea8523b191116beb7
tassosblackg <soylis.k@gmail.com>
2022-11-10 13:31:57 +0200

    mongo-connect docker container

--- mongo_connector.py
+++ mongo_connector.py
@@ -4,15 +4,17 @@ def get_database():
     import pymongo
 
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+    # CONNECTION_STRING = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
     # "mongodb+srv://<username>:<password>@<cluster-name>.mongodb.net/myFirstDatabase"
+    CONNECTION_STRING = 'mongodb://so_close_db'
+    db_name = 'so-close'
 
     # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
     from pymongo import MongoClient
     client = MongoClient(CONNECTION_STRING)
 
     # Create the database for our example (we will use the same database throughout the tutorial
-    return client['so-close']
+    return client[db_name]
 
 
 # This is added so that many files can reuse the function get_database()
